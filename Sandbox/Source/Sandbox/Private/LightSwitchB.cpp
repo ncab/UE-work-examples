055dadaffe0259811b01f03f7788012d0a91033c
ncab <60075021+ncab@users.noreply.github.com>
2023-10-02 16:41:48 -0400

    Update LightSwitchB.cpp

--- Sandbox/Source/Sandbox/Private/LightSwitchB.cpp
+++ Sandbox/Source/Sandbox/Private/LightSwitchB.cpp
@@ -1,6 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "LightSwitchB.h"
 #include "Components/PointLightComponent.h"
 #include "Components/SphereComponent.h"
